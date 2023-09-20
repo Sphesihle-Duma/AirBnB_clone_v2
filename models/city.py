c63c200618a8b0fc457235d3a6d773902127fb1e
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-09-20 16:04:15 -0700

    Added task 6

--- models/city.py
+++ models/city.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
-
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"  # Set the table name to "cities"
+    name = Column(String(128), nullable=False)  # Add the "name" column
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)  # Add the "state_id" column as a foreign key to states.id
---
+++ models/engine/db_storage.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+""" new class for sqlAlchemy """
+import os
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (create_engine)
+from models.base_model import Base
+from models.state import State
+from models.city import City
+
+
+class DBStorage:
+    """ create tables in environmental"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        user = os.getenv("HBNB_MYSQL_USER")
+        passwd = os.getenv("HBNB_MYSQL_PWD")
+        db = os.getenv("HBNB_MYSQL_DB")
+        host = os.getenv("HBNB_MYSQL_HOST")
+        env = os.getenv("HBNB_ENV")
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
+                                      pool_pre_ping=True)
+
+        if env == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        dic = {}
+        if cls:
+            if type(cls) is str:
+                cls = eval(cls)
+            query = self.__session.query(cls)
+            for elem in query:
+                key = "{}.{}".format(type(elem).__name__, elem.id)
+                dic[key] = elem
+        else:
+            lista = [State, City]
+            for clase in lista:
+                query = self.__session.query(clase)
+                for elem in query:
+                    key = "{}.{}".format(type(elem).__name__, elem.id)
+                    dic[key] = elem
+        return (dic)
+
+    def new(self, obj):
+        """add a new element in the table
+        """
+        self.__session.add(obj)
+
+    def save(self):
+        """save changes
+        """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete an element in the table
+        """
+        if obj:
+            self.session.delete(obj)
+
+    def reload(self):
+        """configuration
+        """
+        Base.metadata.create_all(self.__engine)
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sec)
+        self.__session = Session()
+
+    def close(self):
+        """ calls remove()
+        """
+        self.__session.close()
