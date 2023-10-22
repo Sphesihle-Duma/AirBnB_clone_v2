e1e2cf062073e41d1d0895adb773627b81dbccb4
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-10-21 17:21:38 -0700

    test

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """ takes the text and process it """
-    return 'C %s' % text.replace('_', ' ')
+    return f"C {text.replace('_', ' ')}"
 
 
 if __name__ == '__main__':
