b3ba7e88bd2e6ced8b9b71c2cda8dc0ce5ba8193
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-10-21 17:13:29 -0700

    Added 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Creates a flask app that handles three route """
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Returns Hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Return HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """ takes the text and process it """
+    return 'C %s' % text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
