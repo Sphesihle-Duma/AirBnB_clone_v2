7715c65e078150df9f366ed8b63379fe18173ef7
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-10-21 17:52:08 -0700

    testing the fix

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python3
-""" Creates a flask app that handles three route """
+""" Script that runs an app with Flask framework """
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Returns Hello HBNB """
-    return "Hello HBNB!"
+    """ Function called with / route """
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Return HBNB """
-    return "HBNB"
+    """ Function called with /hbnb route """
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """ takes the text and process it """
-    return f"C {text.replace('_', ' ')}"
+    """ Function called with /c/<text> route """
+    return 'C %s' % text.replace('_', ' ')
 
 
 @app.route('/python/', strict_slashes=False)
-@app.route('python/<text>', strict_slashes=True)
-def python_is_cool(text='is cool'):
-    """ takes the text and process it """
-    return f"Python {text.replace('_'), ' '}"
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """ Function called with /python/<text> route """
+    if text is not 'is cool':
+        text = text.replace('_', ' ')
+    return 'Python %s' % text
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
