a03a0e1a9f1e8176de1266e12ac2c9e0df74e171
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-10-21 17:42:01 -0700

    Added 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Creates a flask app that handles three route """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Returns Hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Return HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ takes the text and process it """
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/', strict_slashes)
+@app.route('python/<text>', strict_slashes=True)
+def python_is_cool(text='is cool'):
+    """ takes the text and process it """
+    if text is not 'is cool':
+        text = text.replace('_', ' ')
+    return 'Python %s' % text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
